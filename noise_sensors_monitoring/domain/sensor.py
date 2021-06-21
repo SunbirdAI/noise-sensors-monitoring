132fcf61278a3186f9d0bc09a79a17ca3678dca4
Isaac Owomugisha <archimedesisaac@gmail.com>
2021-06-21 10:59:57 +0300

    add sensor object initialization from dictionary

--- noise_sensors_monitoring/domain/sensor.py
+++ noise_sensors_monitoring/domain/sensor.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+from typing import Dict
 
 @dataclass
 class Sensor:
@@ -9,3 +10,10 @@ class Sensor:
     latitude: float
     batteryLevel: int
     sigStrength: int
+
+    @classmethod
+    def from_dict(self, data: Dict):
+        return self(**data)
+    
+    def to_dict(self):
+        return asdict(self)
--- tests/domain/test_sensor.py
+++ tests/domain/test_sensor.py
@@ -19,3 +19,24 @@ def test_sensor_model_init():
     assert sensor.latitude == 0.564
     assert sensor.batteryLevel == 30
     assert sensor.sigStrength == 26
+
+def test_from_dict_init():
+    init_dict = {
+        "deviceId": "SB1001",
+        "dbLevel": 76,
+        "connected": True,
+        "longitude": 1.034,
+        "latitude": 0.564,
+        "batteryLevel": 30,
+        "sigStrength": 26
+    }
+
+    sensor = Sensor.from_dict(init_dict)
+
+    assert sensor.deviceId == "SB1001"
+    assert sensor.dbLevel == 76
+    assert sensor.connected == True
+    assert sensor.longitude == 1.034
+    assert sensor.latitude == 0.564
+    assert sensor.batteryLevel == 30
+    assert sensor.sigStrength == 26
