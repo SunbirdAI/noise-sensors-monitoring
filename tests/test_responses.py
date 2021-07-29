0862d1a0bec099455064ee18d21a9bb90c8eacca
Isaac Owomugisha <archimedesisaac@gmail.com>
2021-07-30 00:08:41 +0300

    Add Response objects and refactor use case to return Response objects

---
+++ tests/test_responses.py
@@ -0,0 +1,66 @@
+from noise_sensors_monitoring.requests.sensor_reading import SensorReadingInvalidRequest
+from noise_sensors_monitoring.responses import (
+    ResponseTypes,
+    ResponseSuccess,
+    ResponseFailure,
+    build_response_from_invalid_request
+)
+
+SUCCESS_VALUE = {"key": ["value1", "value2"]}
+GENERIC_RESPONSE_TYPE = "Response"
+GENERIC_RESPONSE_MESSAGE = "This is a response"
+
+
+def test_response_success_is_true():
+    response = ResponseSuccess(SUCCESS_VALUE)
+
+    assert bool(response) is True
+
+
+def test_response_success_has_type_and_value():
+    response = ResponseSuccess(SUCCESS_VALUE)
+
+    assert response.type == ResponseTypes.SUCCESS
+    assert response.value == SUCCESS_VALUE
+
+
+def test_response_failure_is_false():
+    response = ResponseFailure(GENERIC_RESPONSE_TYPE, GENERIC_RESPONSE_MESSAGE)
+
+    assert bool(response) is False
+
+
+def test_response_failure_has_type_and_message():
+    response = ResponseFailure(
+        GENERIC_RESPONSE_TYPE, GENERIC_RESPONSE_MESSAGE
+    )
+
+    assert response.type == GENERIC_RESPONSE_TYPE
+    assert response.message == GENERIC_RESPONSE_MESSAGE
+    assert response.value == {
+        "type": GENERIC_RESPONSE_TYPE,
+        "message": GENERIC_RESPONSE_MESSAGE
+    }
+
+
+def test_response_failure_initialisation_with_exception():
+    response = ResponseFailure(
+        GENERIC_RESPONSE_TYPE, Exception("Just an error message")
+    )
+
+    assert bool(response) is False
+    assert response.type == GENERIC_RESPONSE_TYPE
+    assert response.message == "Exception: Just an error message"
+
+
+def test_response_failure_from_invalid_request():
+    request = SensorReadingInvalidRequest({"randomField": "generic", "connected": 7678})
+    request.add_error("invalid field", "randomField is not a valid field")
+    request.add_error("invalid type", "connected should be boolean data type")
+
+    response = build_response_from_invalid_request(request)
+
+    assert bool(response) is False
+    assert response.type == ResponseTypes.INVALID_INPUT_ERROR
+    assert response.message == "invalid field: randomField is not a valid field\n" \
+                               "invalid type: connected should be boolean data type"
