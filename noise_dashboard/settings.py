1deccab3c24b328139ae809681f56100af6bd53b
Isaac Owomugisha <archimedesisaac@gmail.com>
2021-08-03 21:05:30 +0300

    Add users app that implements a custom user model

--- noise_dashboard/settings.py
+++ noise_dashboard/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
