4f978a6f8cecaa1fd28427f98c7b0d3b629bf553
NLSanyu <lydiansanyu@gmail.com>
2021-08-10 22:20:13 +0300

    Add validation for phone number

--- devices/forms.py
+++ devices/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm, ValidationError
 from .models import Device
+import re
 
 
 class DeviceForm(ModelForm):
@@ -13,3 +14,10 @@ class DeviceForm(ModelForm):
             raise ValidationError('IMEI must be 15-digit number')
         return imei
 
+    def clean_phone_number(self, *args, **kwargs):
+        # this particular validation is a placeholder and should be updated later
+        phone_number = self.cleaned_data.get('phone_number')
+        pattern = '^(07)([0-9]{8})$'
+        if not re.match(pattern, phone_number):
+            raise ValidationError('Please enter a valid phone number')
+        return phone_number
