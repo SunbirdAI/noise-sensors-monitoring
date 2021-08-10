76ea62bb876195e6970a88efb72f690275201696
NLSanyu <lydiansanyu@gmail.com>
2021-08-10 14:24:46 +0300

    Validate IMEI number - ensure it is 15 digits

--- devices/forms.py
+++ devices/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ValidationError
 from .models import Device
 
 
@@ -6,3 +6,10 @@ class DeviceForm(ModelForm):
     class Meta:
         model = Device
         exclude = ['id']
+
+    def clean_imei(self, *args, **kwargs):
+        imei = self.cleaned_data.get('imei')
+        if len(imei) != 15:
+            raise ValidationError('IMEI must be 15-digit number')
+        return imei
+
