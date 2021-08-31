a11f73695b7a1e07f969bacf8c617cc64afcf263
Isaac Owomugisha <archimedesisaac@gmail.com>
2021-09-01 01:21:48 +0300

    Setup retrieval of device configuration from django models

---
+++ noise_sensors_monitoring/requests/device_config.py
@@ -0,0 +1,21 @@
+from typing import Dict, Optional
+from noise_sensors_monitoring.requests.generic_requests import InvalidRequest, ValidRequest
+
+
+def build_device_config_request(config_dict: Optional[Dict] = None):
+    invalid_req = InvalidRequest(config_dict)
+    if config_dict is None:
+        invalid_req.add_error("No data", "The configuration request has no data")
+        return invalid_req
+
+    if "imei" not in config_dict:
+        invalid_req.add_error("Missing values", "imei is required.")
+
+    imei = str(config_dict["imei"])
+    if len(imei) != 15 or not imei.isnumeric():
+        invalid_req.add_error("Invalid value", "The imei value should be a 15-digit numeric value")
+
+    if invalid_req.has_errors():
+        return invalid_req
+
+    return ValidRequest(config_dict)
---
+++ noise_sensors_monitoring/use_cases/devices_config.py
@@ -0,0 +1,16 @@
+from noise_sensors_monitoring.repository.devices_repo import DevicesRepo
+from noise_sensors_monitoring.requests.generic_requests import Request
+from noise_sensors_monitoring.responses import (
+    ResponseSuccess, ResponseFailure, ResponseTypes, build_response_from_invalid_request
+)
+
+
+def get_device_config(request: Request, repo: DevicesRepo):
+    if not request:
+        return build_response_from_invalid_request(request)
+
+    device_config = repo.get_device_configuration_by_imei(request.request_dict)
+    if "errors" in device_config:
+        return ResponseFailure(ResponseTypes.INVALID_INPUT_ERROR, device_config["errors"])
+    else:
+        return ResponseSuccess(device_config)
