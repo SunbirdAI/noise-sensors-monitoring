8644b72ac53061c26f2aaa889d12d29030081f6c
Isaac Owomugisha <archimedesisaac@gmail.com>
2021-09-02 18:56:12 +0300

    Modify static file paths to use os. Doing this to trying to fix css-not-loading issues on the deployment

--- noise_dashboard/settings.py
+++ noise_dashboard/settings.py
@@ -140,8 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static', ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
