1ff463c3755ab323664faf5ea205bc72ef2b3718
Isaac Owomugisha <archimedesisaac@gmail.com>
2021-09-02 18:42:06 +0300

    Add static to urlpatterns for use in production environment

--- noise_dashboard/urls.py
+++ noise_dashboard/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Django admin
@@ -28,4 +30,4 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('devices/', include('devices.urls')),
     path('audio/', include('recordings.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
