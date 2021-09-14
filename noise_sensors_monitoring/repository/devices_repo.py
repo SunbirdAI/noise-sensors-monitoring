19ea66302c3a5c710178053b5e1fd48703ae9ecc
Isaac Owomugisha <archimedesisaac@gmail.com>
2021-09-14 16:23:24 +0300

    Remove reading device config from DB

--- noise_sensors_monitoring/repository/devices_repo.py
+++ noise_sensors_monitoring/repository/devices_repo.py
@@ -1,11 +1,11 @@
-import os
-import django
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'noise_dashboard.settings'
-django.setup()
-
-from devices.models import Device
-from django.core.exceptions import ObjectDoesNotExist
+# import os
+# import django
+#
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'noise_dashboard.settings'
+# django.setup()
+#
+# from devices.models import Device
+# from django.core.exceptions import ObjectDoesNotExist
 
 
 class DevicesRepo:
@@ -17,17 +17,20 @@ class DevicesRepo:
         pass
 
     def get_device_configuration_by_imei(self, imei_dict):
-        try:
-            device = Device.objects.get(imei=imei_dict["imei"])
-            return {
-                "configured": device.configured,
-                "deviceId": device.device_id,
-                "mode": device.mode,
-                "recLength": device.recLength,
-                "recInterval": device.recInterval,
-                "uploadAddr": device.uploadAddr
-            }
-        except ObjectDoesNotExist:
-            return {
-                "error": f"No device with imei: {imei_dict['imei']}"
-            }
+        return {
+            "TODO": "Create an API for the device configuration"
+        }
+        # try:
+        #     device = Device.objects.get(imei=imei_dict["imei"])
+        #     return {
+        #         "configured": device.configured,
+        #         "deviceId": device.device_id,
+        #         "mode": device.mode,
+        #         "recLength": device.recLength,
+        #         "recInterval": device.recInterval,
+        #         "uploadAddr": device.uploadAddr
+        #     }
+        # except ObjectDoesNotExist:
+        #     return {
+        #         "error": f"No device with imei: {imei_dict['imei']}"
+        #     }
