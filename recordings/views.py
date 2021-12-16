4c7cf73baf50a663c1b4eff86c0e31583c8328b4
Isaac Owomugisha <archimedesisaac@gmail.com>
2021-12-16 15:02:04 +0300

    Make docker-compose-web.yml a test environment

--- recordings/views.py
+++ recordings/views.py
@@ -9,3 +9,9 @@ class ReceiveAudioViewSet(viewsets.ModelViewSet):
     serializer_class = RecordingSerializer
     parser_classes = [parsers.MultiPartParser]
     http_method_names = ['post']
+
+    # def finalize_response(self, request, response, *args, **kwargs):
+    #     if response.status_code != 201:
+    #         print(response.data)
+    #         response.data = {"Failed"}
+    #     return super(ReceiveAudioViewSet, self).finalize_response(request, response, *args, **kwargs)
