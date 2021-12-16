d349026fae2c7887ddff8c631b9e4b1d4052d4ef
NLSanyu <lydiansanyu@gmail.com>
2021-12-16 09:10:59 +0300

    Create DeviceMetrics model and migrate

--- device_metrics/models.py
+++ device_metrics/models.py
@@ -1,2 +1,26 @@
+import uuid
+
 from django.db import models
+from django.db.models.fields import PositiveIntegerField
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+from devices.models import Device
+
+class DeviceMetrics(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    sig_strength = models.PositiveSmallIntegerField(
+        validators = [MaxValueValidator(31)]
+    )
+    db_level = models.PositiveIntegerField()
+    last_rec = models.PositiveIntegerField()
+    last_upl = models.PositiveIntegerField()
+    panel_voltage = models.PositiveIntegerField()
+    battery_voltage = models.PositiveIntegerField()
+    data_balance = models.PositiveIntegerField()
+    datetime_uploaded = models.DateTimeField(auto_now_add=True)
 
