def4e7966f243c0e304107d72f9613cf81584df1
NLSanyu <lydiansanyu@gmail.com>
2022-01-31 14:51:36 +0300

    Add fetching device metrics for a particular device

--- device_metrics/urls.py
+++ device_metrics/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from .views import ReceiveDeviceMetricsViewSet
+from .views import ReceiveDeviceMetricsViewSet, ListDeviceMetrics
 
 from rest_framework.routers import SimpleRouter
 
@@ -8,5 +8,6 @@ router = SimpleRouter()
 router.register('', ReceiveDeviceMetricsViewSet)
 
 urlpatterns = [
+    path('<slug:device_id>', ListDeviceMetrics.as_view(), name='device_device_metrics'),
     path('', include(router.urls))
 ]
--- device_metrics/views.py
+++ device_metrics/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListAPIView
 
 from .models import DeviceMetrics
 from .serializers import DeviceMetricsSerializer
@@ -9,4 +9,13 @@ from .serializers import DeviceMetricsSerializer
 class ReceiveDeviceMetricsViewSet(viewsets.ModelViewSet):
     queryset = DeviceMetrics.objects.all()
     serializer_class = DeviceMetricsSerializer
-    http_method_names = ['get', 'post']
+
+
+class ListDeviceMetrics(ListAPIView):
+    queryset = DeviceMetrics.objects.all()
+    serializer_class = DeviceMetricsSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(
+            device__device_id=self.kwargs['device_id']
+        )
