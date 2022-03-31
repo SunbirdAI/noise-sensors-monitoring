3c5f0bbe7f2d5134a69c4323aabd7c4d8909c004
Lydia Sanyu Naggayi <37118361+NLSanyu@users.noreply.github.com>
2022-03-31 12:40:40 +0300

    Return day and night aggregations  - Doing this separately (in seperate objects) at first, to test the date filtering functionality

--- analysis/query_influx.py
+++ analysis/query_influx.py
@@ -30,31 +30,49 @@ class InfluxClient:
 
     def query_data(self, query=query):
         query_api = self._client.query_api()
-        query_result = query_api.query(org=self._org, query=query)
-        data = []
-        for table in query_result:
-            for record in table.records:
-                data.append(
-                    (record.get_time(), record.get_value(), record.values.get("deviceId")))
-        self.data = data
+        query_result = []
+        try:
+            query_result = query_api.query(org=self._org, query=query)
+        except Exception as exc:
+            print(f"Error fetching data: {exc}")
+        if not query_result:
+            return None
+        else:
+            data = []
+            for table in query_result:
+                for record in table.records:
+                    data.append(
+                        (record.get_time(), record.get_value(), record.values.get("deviceId")))
+            self.data = data
 
     def aggregate_results(self):
         df = pd.DataFrame(self.data)
         df.rename(columns={0: "datetime", 1: "db_level", 2: "device_id"}, inplace=True)
+        df.set_index("datetime", inplace=True)
+        df.sort_index(inplace=True, ascending=True)
+        df_day = df.between_time("6:00", "22:00")
+        df_night = df.between_time("22:00", "6:00")
+        df_day.name = "day"
+        df_night.name = "night"
+        day = self.calculate_metrics(df_day)
+        night = self.calculate_metrics(df_night)
+        return [day, night]
+
+    def calculate_metrics(self, df):
         grp = df.groupby("device_id", sort=False)
         results = []
         for name, group in grp:
             final = {
                 "device_id": name,
                 "noise_analysis": {
-                    "average": group["db_level"].mean(),
-                    "median": group["db_level"].median(),
-                    "max": group["db_level"].max()
+                    f"{df.name}_time_average": group["db_level"].mean(),
+                    f"{df.name}_time_median": group["db_level"].median(),
+                    f"highest_{df.name}_noise": group["db_level"].max(),
+                    "night_time_quiet_hours": ""
                 }
             }
             results.append(final)
         return results
-
 
 if __name__ == "__main__":
     client = InfluxClient()
