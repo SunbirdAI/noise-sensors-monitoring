c54e6f1f8cd5d0690ea401190fa52d2bc7cda3cd
Lydia Sanyu Naggayi <37118361+NLSanyu@users.noreply.github.com>
2022-04-28 13:13:59 +0300

    Customize exceedances for each location category

--- analysis/query_influx.py
+++ analysis/query_influx.py
@@ -4,7 +4,7 @@ import pandas as pd
 from dotenv import load_dotenv
 from influxdb_client import InfluxDBClient
 
-from devices.models import Device
+from devices.models import Device, location_category_information
 
 load_dotenv()
 
@@ -66,20 +66,24 @@ class InfluxClient:
         grouped_data = self.df.groupby("device_id", sort=False)
         aggregated_data = []
         for name, group in grouped_data:
+            if name not in self.locations:
+                continue
             day = group.between_time("6:00", "22:00", inclusive="left")
             night = group.between_time("22:00", "6:00", inclusive="left")
-            day_time_average = day_time_median = highest_day_noise = 0
-            night_time_average = night_time_median = highest_night_noise = 0
+            day_time_average = day_time_median = highest_day_noise = day_time_exceedances = 0
+            night_time_average = night_time_median = highest_night_noise = night_time_exceedances = 0
+            day_threshold = location_category_information[self.locations[name].category]["day_limit"]
+            night_threshold = location_category_information[self.locations[name].category]["night_limit"]
             if not day.empty:
                 day_time_average = day["db_level"].mean()
                 day_time_median = day["db_level"].median()
                 highest_day_noise = day["db_level"].max()
-                day_time_exceedances = len(day[day["db_level"] > 70])
+                day_time_exceedances = len(day[day["db_level"] > day_threshold])
             if not night.empty:
                 night_time_average = night["db_level"].mean()
                 night_time_median = night["db_level"].median()
                 highest_night_noise = night["db_level"].max()
-                night_time_exceedances = len(day[day["db_level"] > 70])
+                night_time_exceedances = len(night[night["db_level"] > night_threshold])
             final = {
                 "location": {
                     "city": self.locations[name].city,
@@ -89,12 +93,12 @@ class InfluxClient:
                     "noise_analysis": {
                         "day_time_average": day_time_average,
                         "day_time_median": day_time_median,
+                        "day_time_exceedances": day_time_exceedances,
                         "highest_day_noise": highest_day_noise,
                         "night_time_average": night_time_average,
                         "night_time_median": night_time_median,
-                        "highest_night_noise": highest_night_noise,
-                        "day_time_exceedances": day_time_exceedances,
                         "night_time_exceedances": night_time_exceedances,
+                        "highest_night_noise": highest_night_noise,
                         "night_time_quiet_hours": ""
                     }
                 }
