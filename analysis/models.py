dbf31d9efa25346e3bf02d91bedafab99c949078
Lydia Sanyu Naggayi <37118361+NLSanyu@users.noreply.github.com>
2022-10-17 20:28:09 +0100

    Update analysis models

--- analysis/models.py
+++ analysis/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.utils import timezone
 
 from devices.models import Device
 
@@ -10,7 +11,24 @@ class DailyAnalysis(models.Model):
         default=uuid.uuid4,
         editable=False
     )
-    daily_avg_db_level = models.FloatField()
-    daily_max_db_level = models.FloatField()
-    daily_no_of_exceedances = models.PositiveIntegerField()
+    date_analyzed = models.DateTimeField(default=timezone.now)
+    daily_avg_db_level = models.FloatField(null=True)
+    daily_median_db_level = models.FloatField(null=True)
+    daily_max_db_level = models.FloatField(null=True)
+    daily_no_of_exceedances = models.PositiveIntegerField(null=True)
+    device = models.OneToOneField(Device, on_delete=models.CASCADE)
+
+
+class HourlyAnalysis(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    date_analyzed = models.DateTimeField(default=timezone.now)
+    hour_analyzed = models.PositiveIntegerField(null=True)
+    hourly_avg_db_level = models.FloatField(null=True)
+    hourly_median_db_level = models.FloatField(null=True)
+    hourly_max_db_level = models.FloatField(null=True)
+    hourly_no_of_exceedances = models.PositiveIntegerField(null=True)
     device = models.OneToOneField(Device, on_delete=models.CASCADE)
--- analysis/views.py
+++ analysis/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .query_influx import InfluxClient
+from .aggregate_influx_data import InfluxClient
 
 class AnalysisView(APIView):
 
