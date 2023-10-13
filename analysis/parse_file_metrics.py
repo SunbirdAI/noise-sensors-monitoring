877e5d9556f3bf174063bb93ed0abf5b3e794590
Isaac Owomugisha <archimedesisaac@gmail.com>
2023-10-13 16:44:07 +0300

    Handle missing decibel values

--- analysis/parse_file_metrics.py
+++ analysis/parse_file_metrics.py
@@ -3,6 +3,7 @@ def parse_file(file, device_id):
     Read in the file, parse it and return a list of the relevant metrics
     """
     metrics = []
+    missing_values = 0
     with file.open('r') as f:
         lines = f.readlines()
         for line in lines:
@@ -10,7 +11,11 @@ def parse_file(file, device_id):
             metrics_list = str(line).split(",")
             decibel_string = metrics_list[0]
             time_uploaded_string = metrics_list[1]
-            decibel = float(decibel_string.split(":")[1])
-            time_uploaded = time_uploaded_string[3:]
-            metrics.append({'device_id': device_id, 'db_level': decibel, 'date': time_uploaded})
+            try:
+                decibel = float(decibel_string.split(":")[1])
+                time_uploaded = time_uploaded_string[3:]
+                metrics.append({'device_id': device_id, 'db_level': decibel, 'date': time_uploaded})
+            except ValueError as e:
+                missing_values += 1
+                print(f"Error: {e}")
     return metrics
